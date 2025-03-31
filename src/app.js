31949ecc6046523664e261eef966c11f6b15958d
ToninoJoya <antoniocaprab@gmail.com>
2025-03-31 20:22:08 +0000

    ejercicio dominios

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //Crear todas las combinaciones posibles con los arrays disponibles y su dominio unico. 
+  // Ej: thegreatjogger.com
+
+  let pronoun = ['the', 'our'];
+  let adjetivo = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+  const dominio = () => {
+    for (let pro of pronoun){
+        for(let adj of adjetivo){
+          for(let n of noun){
+            return `${pro}${adj}${n}.com`
+          }
+        }
+    }
+  }
+
+  let dom = document.querySelector(".dominio")
+  dom.innerHTML = dominio()
+  
 };
